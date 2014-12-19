8ec65e8a8347426243fbbbfcdedced9b8c5b62c6
Timothy McRoy <timothy-mcroy@uiowa.edu>
2014-12-18 23:45:44 -0600

    forgot that I changed main's name

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <utility>
+#include <iomanip>
+#include "characterClass.h"
+#include "gameboard.h"
+#include "Game.h"
+#include "Game.cpp"
+int main()
+{
+    Game first;
+    return 0;
+}
